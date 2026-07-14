b5df6329f75f3daf2e856aad74034b640ea5a55c
Casanova-ctrl <johnathan.casanova92@gmail.com>
2026-07-14 13:55:32 -0400

    Improved Card class by adding type hints

--- card.py
+++ card.py
@@ -1,9 +1,9 @@
 class Card:
 
-    def __init__(self, suit, rank, value):
+    def __init__(self, suit: str, rank: str, value: int):
         self.suit = suit
         self.rank = rank
         self.value = value
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.rank} of {self.suit}"
