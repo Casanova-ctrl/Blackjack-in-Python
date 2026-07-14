cd534b6ee62d3c8c494479d3a3316c6fc5ffb47a
Casanova-ctrl <johnathan.casanova92@gmail.com>
2026-07-14 15:32:01 -0400

    Improved Deck class by adding class constants and class attributes

--- deck.py
+++ deck.py
@@ -2,39 +2,38 @@ import random
 from card import Card
 
 class Deck:
-    def __init__(self):
-        self.cards = []
-        self.suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
-        self.ranks = [
-            "Ace", "2", "3", "4", "5", "6",
-            "7", "8", "9", "10",
-            "Jack", "Queen", "King"
-        ]
-        self.values = {
-            "Ace": 11,
-            "2": 2,
-            "3": 3,
-            "4": 4,
-            "5": 5,
-            "6": 6,
-            "7": 7,
-            "8": 8,
-            "9": 9,
-            "10": 10,
-            "Jack": 10,
-            "Queen": 10,
-            "King": 10
-        }       
+    SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    RANKS = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+    VALUES = {
+        "Ace": 11,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "10": 10,
+        "Jack": 10,
+        "Queen": 10,
+        "King": 10
+    }
+
+    def __init__(self) -> None:
+        self.cards: list[Card] = []      
         self.build_deck()
 
-    def build_deck(self):
-        for suit in self.suits:
-            for rank in self.ranks:
-                value = self.values[rank]
-                self.cards.append(Card(suit, rank, value))
+    def build_deck(self) -> None:
+        for suit in self.SUITS:
+            for rank in self.RANKS:               
+                self.cards.append(Card(suit, rank, self.VALUES[rank]))
 
-    def shuffle(self):
+    def shuffle(self) -> None:
         random.shuffle(self.cards)
 
-    def deal_card(self):
+    def deal_card(self) -> Card | None:
         return self.cards.pop() if self.cards else None
+    
+    def __len__(self) -> int:
+        return len(self.cards)
